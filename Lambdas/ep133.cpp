5f73e732742c9125d83148b7e3dee94e51a0d480
cucui <cuic3@mail2.sysu.edu.cn>
2021-09-06 22:35:29 +0200

    Cplusplus Weekly implementing lambda generate a Fibonacci sequence using lambda

---
+++ Lambdas/ep133.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Cu Cui on 2021/9/6.
+//
+// C++ Weekly - Ep 133 - What Exactly IS A Lambda Anyhow?
+// implement lambda
+
+#include<cstdio>
+#include<iostream>
+
+// implement lambda
+struct my_lambda {
+    my_lambda(int a) {val = a;}
+    int val;
+
+    template<typename T>
+    constexpr auto operator()(T i) {
+        std::cout << "Hello from my lambda!\n";
+        return ++val + i;
+    }
+};
+
+
+int main()
+{
+    int val = 10;
+    my_lambda l(val);
+    auto f = [val](auto i) mutable {std::cout << "Hello from lambda!\n"; return ++val + i;};
+
+    std::cout << "lambda result:\n"    << f(2.1) << std::endl
+              << "my lambda result:\n" << l(2.1) << std::endl;
+
+    return 0;
+}
---
+++ Lambdas/ep37.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Cu Cui on 2021/9/6.
+//
+// C++ Weekly - Ep 37 - Stateful Lambdas
+// generate a Fibonacci sequence using lambda
+
+#include<iostream>
+
+// show how std::exchange works
+void helper_function()
+{
+    int i = 1, j = 2;
+    // j -> i, old_i -> j
+    j = std::exchange(i, j);
+    std::cout << "i = " << i << ", j = " << j << std::endl;
+}
+
+int main()
+{
+    helper_function();
+    int count = 0;
+    auto fibonacci = [i = 0, j = 1, &count]() mutable {
+        i = std::exchange(j, i+j);
+        count++;
+        return j;};
+
+    for (int i=0; i<10; i++)
+        fibonacci();
+    std::cout << "Count: " << count+1 << "\n" << fibonacci() << std::endl;
+
+    return 0;
+}
+
